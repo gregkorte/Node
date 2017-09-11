fc3d8677f3eb67f2f27b111ae74f8e72eb4f2b67
Greg Korte <gregmkorte@gmail.com>
2017-09-11 12:40:10 -0500

    Added sample database to exercise.

--- foundations/12-sqlite-create-db/sqlite3Intro.js
+++ foundations/12-sqlite-create-db/sqlite3Intro.js
@@ -2,8 +2,8 @@
 
 // Creating the database
 const { Database } = require('sqlite3').verbose();
-// const db = new Database('example.sqlite', () => console.log('Connected!'));
-const db = new Database('db/example.sqlite'); // Unable to open database file
+const db = new Database('example.sqlite', () => console.log('Connected!'));
+// const db = new Database('db/example.sqlite'); // Unable to open database file
 
 // Error handler
 const errorHandler = (err) => {
@@ -16,20 +16,20 @@ const errorHandler = (err) => {
 db.run('CREATE TABLE IF NOT EXISTS employees (id INT, first TEXT, last TEXT)', errorHandler);
 
 // Simple INSERT statements
-// db.run('INSERT INTO employees (id, first, last) VALUES (1, "Michael", "Scott")', errorHandler);
-// db.run('INSERT INTO employees VALUES (2, "Jim", "Halpert")', errorHandler);
+db.run('INSERT INTO employees (id, first, last) VALUES (1, "Michael", "Scott")', errorHandler);
+db.run('INSERT INTO employees VALUES (2, "Jim", "Halpert")', errorHandler);
 
-// // Dynamic inserts with javascript
-// const employeeArray = [
-//     { id: 3, firstName: 'Dwight', lastName: 'Schrute' },
-//     { id: 4, firstName: 'Andy', lastName: 'Bernard' },
-//     { id: 5, firstName: 'Pam', lastName: 'Beesly' }
-// ];
+// Dynamic inserts with javascript
+const employeeArray = [
+    { id: 3, firstName: 'Dwight', lastName: 'Schrute' },
+    { id: 4, firstName: 'Andy', lastName: 'Bernard' },
+    { id: 5, firstName: 'Pam', lastName: 'Beesly' }
+];
 
-// // Iterate over array to insert into database
-// employeeArray.forEach((obj) => {
-//     db.run(`INSERT INTO employees VALUES (${obj.id}, "${obj.firstName}", "${obj.lastName}")`, errorHandler)
-// });
+// Iterate over array to insert into database
+employeeArray.forEach((obj) => {
+    db.run(`INSERT INTO employees VALUES (${obj.id}, "${obj.firstName}", "${obj.lastName}")`, errorHandler)
+});
 
 // Query the database
 db.all('SELECT * FROM employees', (err, allRows) => {
